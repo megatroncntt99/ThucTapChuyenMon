ec80b8d92f8b01c31aee753dcdc48454d12a314c
megatroncntt99 <van330055@gmail.com>
2020-07-13 15:53:29 +0700

    del

--- AppNhacVuiVe/app/src/main/java/com/example/Service/APIService.java
+++
@@ -1,9 +0,0 @@
-package com.example.Service;
-
-public class APIService {
-    private static final String base_url="http://192.168.0.146/server/";
-
-    public static DataService getService(){
-        return APIRetrofitClient.getClient(base_url).create(DataService.class);
-    }
-}
