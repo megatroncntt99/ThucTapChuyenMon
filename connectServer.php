676f2571d7289c9f778a408159d33247358b1aba
megatroncntt99 <67847186+megatroncntt99@users.noreply.github.com>
2020-07-17 22:30:51 +0700

    Update connectServer.php

--- connectServer.php
+++ connectServer.php
@@ -1,8 +1,8 @@
 <?php
     $hostname="localhost";
-    $username="root";
-    $pass="";
-    $databasename="appmucis";
+    $username="id14115318_megatron99";
+    $pass="0384374598Van!";
+    $databasename="id14115318_appmusic";
     $con=mysqli_connect($hostname,$username,$pass,$databasename);
     mysqli_query($con,"SET NAMES 'utf8'");
     
